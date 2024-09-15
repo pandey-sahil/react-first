3b9ad1a443ae18cf19b0988d352936eab26c2dee
pandey-sahil <sahilp2593@gmail.com>
2024-09-15 18:32:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+let a = "5";
+
+const App = () => {
+  return (
+    <>
+      <div className='flex justify-center items-center bg-gray-800 h-[80vh] text-blue-600  flex-col leading-none tracking-tight'>
+        <h1 className='text-7xl font-bold uppercase'>React App</h1>
+        <br />
+        <h2 className='text-3xl'> {import.meta.env.VITE_API_KEY}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
