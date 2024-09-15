7a17b8c98ffe2b0218bba9ce1e712a057fcd408a
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-09-15 19:24:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from outside the container
+    port: process.env.PORT || 3000, // Uses Render's PORT or defaults to 3000 for local dev
+  },
 })
